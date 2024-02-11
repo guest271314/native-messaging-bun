0e47e11042bcf86fe1b07c01ef03674a66043d27
guest271314 <guest271314@gmail.com>
2024-02-10 22:53:58 -0800

    Substitute Bun API's for Node.js API's

--- nm_bun.js
+++ nm_bun.js
@@ -1,66 +1,55 @@
-#!/usr/bin/env -S ./bun run --no-install --hot
+#!/usr/bin/env -S ./bun run --no-install --smol
 // Bun Native Messaging host
 // guest271314, 10-9-2022
-async function getMessage() {
-  const { promise, resolve } = Promise.withResolvers();
-  // https://github.com/simov/native-messaging/blob/8e99d2a345ae94426a502d05aa5d57b966f6bc78/protocol.js
-  let messageLength = 0,
-    bytesWritten = 0,
-    input = [];
 
-  process.stdin.on("readable", () => {
-    let chunk;
-    while ((chunk = process.stdin.read())) {
-      // Set message value length once
-      if (messageLength === 0) {
-        [messageLength] = new Uint32Array(chunk.buffer.slice(0, 4));
-        chunk = chunk.subarray(4);
+const encoder = new TextEncoder();
+
+function encodeMessage(message) {
+  return encoder.encode(JSON.stringify(message));
+}
+
+async function* getMessage() {
+  const buffer = new ArrayBuffer(0, { maxByteLength: 1048576 });
+  const view = new DataView(buffer);
+  let messageLength = 0;
+  let readOffset = 0;
+  for await (let message of Bun.stdin.stream()) {
+    if (buffer.byteLength === 0) {
+      buffer.resize(4);
+      for (let i = 0; i < 4; i++) {
+        view.setUint8(i, message[i]);
       }
-      // Store accrued message length read
-      bytesWritten += chunk.length;
-      input.push(...chunk);
-      if (bytesWritten === messageLength) {
-        // Send accrued message from client back to client
-        resolve(new Uint8Array(input));
-      }
+      messageLength = view.getUint32(0, true);
+      message = message.subarray(4);
+      buffer.resize(0);
     }
-  });
-
-  return await promise;
-}
-
-function sendMessage(json) {
-  // https://github.com/denoland/deno/discussions/17236#discussioncomment-4566134
-  const header = new Uint32Array([json.length]);
-  /*
-  // Long form
-  const header = new Uint32Array([
-    ((uint32) =>     
-      // https://stackoverflow.com/a/58288413
-      (uint32[3] << 24) 
-      | (uint32[2] << 16) 
-      | (uint32[1] << 8) 
-      | (uint32[0])
-      )(Array.from({
-        length: 4,
-      }, (_,index)=>(json.length >> (index * 8)) & 0xff)
-    )
-  ]);
-  */
-  process.stdout.write(new Uint8Array(header.buffer));
-  process.stdout.write(json);
-  Bun.gc(true);
-}
-
-async function main() {
-  while (true) {
-    try {
-      const message = await getMessage();
-      sendMessage(message);
-    } catch (e) {
-      process.exit();
+    buffer.resize(buffer.byteLength + message.length);
+    for (let i = 0; i < message.length; i++, readOffset++) {
+      view.setUint8(readOffset, message[i]);
+    }
+    if (buffer.byteLength === messageLength) {
+      yield new Uint8Array(buffer);
+      messageLength = 0;
+      readOffset = 0;
+      buffer.resize(0);
     }
   }
 }
 
-main();
+async function sendMessage(message) {
+  const header = Uint32Array.from({
+    length: 4,
+  }, (_, index) => (message.length >> (index * 8)) & 0xff);
+  const output = new Uint8Array(header.length + message.length);
+  output.set(header, 0);
+  output.set(message, 4);
+  await Bun.write(Bun.stdout, output);
+}
+
+try {
+  for await (const message of getMessage()) {
+    await sendMessage(message);
+  }
+} catch (e) {
+  process.exit();
+}
